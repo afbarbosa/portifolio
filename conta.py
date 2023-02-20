185e7e64287173959a6d6097d2982cf62238b371
afbarbosa <40150815+afbarbosa@users.noreply.github.com>
2023-02-20 17:10:08 -0300

    Iniciando em OO

---
+++ conta.py
@@ -0,0 +1,7 @@
+class Conta:
+    def __init__(self, numero, titular, saldo, limite):
+        Conta.numero = numero
+        Conta.titular = titular
+        Conta.saldo = saldo
+        Conta.limite = limite
+        
