0f8cfb33b6957d86fe3255ba33d36daacc969a48
afbarbosa <40150815+afbarbosa@users.noreply.github.com>
2023-02-27 21:29:31 -0300

    Exercicio de classes

---
+++ datas.py
@@ -0,0 +1,8 @@
+class Data:
+    def __init__(self,dia,mes,ano):
+        self.dia
+        self.mes
+        self.ano
+    
+    def formatada(self):
+        print("{}/{}/{}".format(self.dia,self.mes,self.ano))
