b54001a981f9c4a8498a7092ca839dd33d47c92a
afbarbosa <40150815+afbarbosa@users.noreply.github.com>
2023-02-27 21:18:07 -0300

    Inclusão de metodos a classe

--- conta.py
+++ conta.py
@@ -5,3 +5,11 @@ class Conta:
         Conta.saldo = saldo
         Conta.limite = limite
         
+    def extrato (self):
+        print("O valor do extrato é {}".format(self.saldo))
+    
+    def saque (self, valor):
+        self.saldo -= valor
+        
+    def deposita (self, valor):
+        self.saldo += valor          
