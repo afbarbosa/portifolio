028f5fbc93268c21ecc4b0b2b8db35277205ce1f
afbarbosa <40150815+afbarbosa@users.noreply.github.com>
2023-03-07 18:03:09 -0300

    Mudança para self no metodo transfere

--- conta.py
+++ conta.py
@@ -14,6 +14,6 @@ class Conta:
     def deposita (self, valor):
         self.__saldo += valor
 
-    def tranfere (self, valor, origem, destino):
-        origem.saca(valor)
+    def transfere (self, valor, destino):
+        self.saca(valor)
         destino.deposita(valor)
